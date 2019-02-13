30dd0e2244c5c532e3291adb8807b58ff167b1da
Dr Umesh R Bilagi <pradyumnabilagi12@gmail.com>
2019-02-13 11:31:32 +0530

    route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Home from "./components/Home"
 import CurNavbar from "./components/nav/navbar";
 import CurLogin from "./components/users/login"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route} from "react-router-dom";
 
 import './App.css';
 
@@ -10,7 +12,14 @@ class App extends Component {
     return (
       <div className="App">
         <CurNavbar />
-        <CurLogin />
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={CurLogin}/>
+          </div>
+
+        </BrowserRouter>
+    
       </div>
     );
   }
