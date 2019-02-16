c0353e86c305f25b32916ae20d2321345ca4d1b8
umeshramya <umeshbilagi@gmail.com>
2019-02-17 00:14:18 +0530

    repired login and node modules

--- server.js
+++ server.js
@@ -2,24 +2,41 @@ const express = require("express")
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jwt-login");
- const app = express();
- app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
+
+
+const SECRET="GGHHFfgfahak123"
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin : "http:/localhost:3000",
     credentials : true
 }));
 
+// Login Route
 app.post("/login", (req, res)=>{
     let user = req.body.user;
     let password = req.body.password;
     let login=0;
-    if(user === password){
-       jwt.sign(req, res, user, "TPO", 1);
+    try {
+        if(user === password){
+            jwt.sign(req, res, user, SECRET, 120);
+         }else{
+             throw "Invalid Login"
+         }
+        
+    } catch (error) {
+        res.status(500).send(error)
+        
     }
-    
-    // res.send("L:L:L:L:Luiuiu").end();
-    // res.status(200).json()
+
+
+
 })
+// logout route
+app.get("/logout", function(req, res){
+    jwtLogin.signout(req, res, false);
+});
  app.listen(9000);
 //  require("./users/login");
 
