c4ee9f0c9ebeb5c5a6650bee16f748ed257dd7b8
Pradyumna <pradyumnabilagi12@gmail.com>
2019-02-17 19:08:16 +0530

    login context

--- client/src/components/commons/context.js
+++ client/src/components/commons/context.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import $ from "jquery"
 export const AppContext = React.createContext();
+
 
 export class AppProvider extends React.Component {
     state={
-        loggedState : "Logout"
-        // ,userLogin  : (e)=>{
-        //   e.preventDefault();
-        //   // write code to server /login (post) route
-        //   let body ={"user" : this.state.user, "password" : this.state.password};
-        //   $.ajax({
-        //     method : "POST",
-        //     url : "/login",
-        //     data : body,
-        //   }).done((data)=>{
-        //     this.setState.loggedState = "Login"
-        //   }).catch((er)=>{
-        //     this.setState.loggedState = "Logout"
-        //   })
+        loggedState : "Logout",
+        user : "",
+        password :"",
+        setUser : (e)=>{
+          this.setState({
+            user : e.target.value
+          });
+        },
+        setPassword : (e)=>{
+          this.setState({
+            password : e.target.value
+          });
+        },
+        userLogin  : (e)=>{
+          e.preventDefault();
+          // write code to server /login (post) route
+          let body ={"user" : this.state.user, "password" : this.state.password};
+          $.ajax({
+            method : "POST",
+            url : "/login",
+            data : body,
+          }).done((data)=>{
+            
+            this.setState({
+              loggedState : "Login"
+            });
+          }).catch((er)=>{
+            this.setState({
+              loggedState : "Logout"
+            });
            
-        // }
+          })
+           
+        }
     }
   render() {
     return (
--- client/src/components/users/login.js
+++ client/src/components/users/login.js
@@ -1,62 +1,69 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, Container, Row, Col} from 'reactstrap';
-import $ from "jquery"
+
+import {AppContext} from "../commons/context"
 
 export default class CurLogin extends Component {
-  state={
-    user : "",
-    password : ""
-  }
+  // state={
+  //   user : "",
+  //   password : ""
+  // }
 
-  setUser=(e)=>{
-    this.setState({
-      user : e.target.value
-    });
-  }
+  // setUser=(e)=>{
+  //   this.setState({
+  //     user : e.target.value
+  //   });
+  // }
 
-  setPassword = (e)=>{
-    this.setState({
-      password : e.target.value
-    });
-  }
+  // setPassword = (e)=>{
+  //   this.setState({
+  //     password : e.target.value
+  //   });
+  // }
 
-  userLogin =(e)=>{
-    e.preventDefault();
-    // write code to server /login (post) route
-    let body ={"user" : this.state.user, "password" : this.state.password};
-    $.ajax({
-      method : "POST",
-      url : "/login",
-      data : body,
-    }).done((data)=>{
-      console.log(data);
-    }).catch((er)=>{
-      console.log(er.responseText);
-    })
+  // userLogin =(e)=>{
+  //   e.preventDefault();
+  //   // write code to server /login (post) route
+  //   let body ={"user" : this.state.user, "password" : this.state.password};
+  //   $.ajax({
+  //     method : "POST",
+  //     url : "/login",
+  //     data : body,
+  //   }).done((data)=>{
+  //     console.log(data);
+  //   }).catch((er)=>{
+  //     console.log(er.responseText);
+  //   })
      
-  }
+  // }
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col sm="12" md={{ size: 6, offset: 3 }}>
-                <h3>Login</h3>
-                <Form action="/login" method="post">
-                  <FormGroup>
-                    <Label for="user">User Name</Label>
-                    <Input type="text" name="user" id="user" placeholder="User Name" onChange={this.setUser}/>
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="password">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="Password" onChange={this.setPassword}/>
-                  </FormGroup>
-                  <Button color="info" onClick={this.userLogin}>Login</Button>{' '}
-                </Form>
-            </Col>
-          </Row>
-        </Container>        
-      </div>
+      <AppContext.Consumer>
+        {
+          (c)=>{
+            return <div>
+              <Container>
+                <Row>
+                  <Col sm="12" md={{ size: 6, offset: 3 }}>
+                      <h3>Login</h3>
+                      <Form action="/login" method="post">
+                        <FormGroup>
+                          <Label for="user">User Name</Label>
+                          <Input type="text" name="user" id="user" placeholder="User Name" onChange={c.setUser}/>
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="password">Password</Label>
+                          <Input type="password" name="password" id="password" placeholder="Password" onChange={c.setPassword}/>
+                        </FormGroup>
+                        <Button color="info" onClick={c.userLogin}>Login</Button>{' '}
+                      </Form>
+                  </Col>
+                </Row>
+              </Container>        
+            </div>
+          }
+        }
+      </AppContext.Consumer>
     )
   }
 }
