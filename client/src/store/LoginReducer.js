1efea5439a0952f7a525dc7cee818d29cb02fd1f
Dr Umesh R Bilagi <pradyumnabilagi12@gmail.com>
2019-03-02 12:03:56 +0530

    diff

--- client/src/store/LoginReducer.js
+++ client/src/store/LoginReducer.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-
-
-const intialState = {
-    logged : true,
-=======
 import $ from "jquery"
 
 const intialState = {
     logged : false,
     alertErrorLogged : false,
->>>>>>> ed71380eed70413a4aef8b0ea65ad0aeb53e9aef
     user : "",
     role : "",
     responceText : ""
@@ -20,10 +13,6 @@ const reducer = (state=intialState, action)=>{
     const newState = {...state};
    
     if(action.type === "LOGIN"){
-<<<<<<< HEAD
-        alert(action.payload);
-        newState.logged = action.payload;
-=======
         if(action.payload.logged == false){
             $.ajax({
                 method : "GET",
@@ -36,7 +25,6 @@ const reducer = (state=intialState, action)=>{
         newState.responceText = action.payload.responceText;
         newState.role = action.payload.role;
         newState.alertErrorLogged = action.payload.alertErrorLogged;
->>>>>>> ed71380eed70413a4aef8b0ea65ad0aeb53e9aef
     }
 
     if(action.type === "LOGOUT"){
