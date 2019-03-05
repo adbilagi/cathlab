7378d15bb72f283dca1a4592c8687187614d3c8d
Dr Umesh R Bilagi <pradyumnabilagi12@gmail.com>
2019-03-05 14:23:18 +0530

    side bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CurNavbar from "./components/nav/navbar";
 import CurRoutes from "./components/commons/CurRoutes"
+import SideBar from "./components/commons/SideBar"
+import {Container, Row, Col} from "reactstrap"
 
 
 
@@ -15,8 +17,19 @@ class App extends Component {
     return (
      
       <div className="App">
+      <Container>
         <CurNavbar />
-        <CurRoutes />
+        <Row>
+          <Col xs="12" sm="3" lg="2"><SideBar /></Col>
+        <Col><CurRoutes /></Col>
+        
+
+        </Row>
+
+
+      </Container>
+        
+
       </div>
 
     );
---
+++ client/src/components/commons/Modal.js
@@ -0,0 +1,41 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class ModalExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button  width="90%" color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.props.title}</ModalHeader>
+          <ModalBody>
+           {this.props.container}
+          </ModalBody>
+          {/* <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter> */}
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalExample;
