df449ef9c5c267e601a429e3c2059abe74f1475b
Dr Umesh R Bilagi <pradyumnabilagi12@gmail.com>
2019-03-08 14:10:06 +0530

    route of group

--- routes/api/group.js
+++ routes/api/group.js
@@ -11,20 +11,23 @@ const validate = require("../../validate/groupValidate");
 
 module.exports =router;
 
-router.post("/create", jwt.validateLogin, roleMiddleware, (req, res)=>{
+
+// @route POST
+// Description : creating new group name
+router.post("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
     try {
-        if(!req.permission){
+        if(!req.permission){// check permisiion
             res.status(500).send("You do not have permission for this acction");
             return;
         }
-        validate(req.body);
+        validate(req.body);// validate post data
 
-        conGroup.create({
+        conGroup.create({// create new doc in mongodb
             name : req.body.name,
             underGroup : req.body.underGroup
         }, (err, data)=>{
             if(err){
-                res.status(500).send("Could not creat new group");
+                res.status(500).send("Could not creat new group");/
                 return;
             }else{
                 res.status(200).json({
@@ -38,4 +41,63 @@ router.post("/create", jwt.validateLogin, roleMiddleware, (req, res)=>{
         res.status(500).send(error);
         
     }
-} )
+} );
+
+// @route GET
+// Descrption This is for getting specified group
+router.get("/:group", jwt.validateLogin, roleMiddleware, (req, res)=>{
+    try {
+        if(!req.permission){
+            res.status(500).send("you do not have permission for this action");
+            return;
+        }
+        
+        conGroup.find({
+            name : req.body.params.group
+        },(err, data)=>{
+            if(err){
+                res.status(500).send(err);
+                return
+            }else{
+                res.status(200).json({data : data, message : "Kindly check the recived group name"});
+                return
+            }
+        })
+        
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+});
+
+// @route PUT
+// description  alters the group
+
+router.put("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
+    try {
+        if(!req.permission){
+            res.status(500).send("you do not have permission for this action");
+            return;
+        }
+        validate(req.body);
+        conGroup.updateOne({name : req.body.oldName}, 
+            {name : req.body.newName, underGroup : req.body.underGroup},
+            (err, raw)=>{
+                if(raw.n <1){
+                    res.status(500).send("Could not modify any record");
+                    return;
+                }else if(err){
+                    res.status(500).send(err);
+                    return;
+                }else{
+                    res.status(200).json({data : raw, message : "Succefully  edited group"});
+                    return;
+                }
+                
+            })
+        
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+});
