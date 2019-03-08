4ac79a0e417e4f8f34f13a5eaeece235dbc64edd
Pradyumna <pradyumnabilagi12@gmail.com>
2019-03-08 22:58:16 +0530

    delete route for group

--- routes/api/group.js
+++ routes/api/group.js
@@ -27,7 +27,7 @@ router.post("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
             underGroup : req.body.underGroup
         }, (err, data)=>{
             if(err){
-                res.status(500).send("Could not creat new group");/
+                res.status(500).send("Could not creat new group");
                 return;
             }else{
                 res.status(200).json({
@@ -51,9 +51,10 @@ router.get("/:group", jwt.validateLogin, roleMiddleware, (req, res)=>{
             res.status(500).send("you do not have permission for this action");
             return;
         }
+        let groupName = req.body.params.group;
         
         conGroup.find({
-            name : req.body.params.group
+            name : groupName
         },(err, data)=>{
             if(err){
                 res.status(500).send(err);
@@ -72,7 +73,6 @@ router.get("/:group", jwt.validateLogin, roleMiddleware, (req, res)=>{
 
 // @route PUT
 // description  alters the group
-
 router.put("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
     try {
         if(!req.permission){
@@ -81,7 +81,7 @@ router.put("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
         }
         validate(req.body);
         conGroup.updateOne({name : req.body.oldName}, 
-            {name : req.body.newName, underGroup : req.body.underGroup},
+            {name : req.body.name, underGroup : req.body.underGroup},
             (err, raw)=>{
                 if(raw.n <1){
                     res.status(500).send("Could not modify any record");
@@ -93,11 +93,76 @@ router.put("/", jwt.validateLogin, roleMiddleware, (req, res)=>{
                     res.status(200).json({data : raw, message : "Succefully  edited group"});
                     return;
                 }
-                
             })
         
     } catch (error) {
         res.status(500).send(error);
     }
 
+});
+
+// @route DELETE
+// Descrpition : This route is meant for delete groups 
+router.delete("/:group", jwt.validateLogin, roleMiddleware, (req, res)=>{
+    try {
+        if(!req.permission){
+            res.status(500).send("you do not have permission for this action");
+            return;
+        }
+        let groupName = req.body.params.group;
+        // write code to check for checking under group  and ledger existance before deleting if so one can delete group
+        conGroup.find({// check under group before deleting
+            underGroup : groupName
+        }, (err, data)=>{
+            if(err){
+                res.status(500).send(err);
+            }else{
+                if(data[0].underGroup == groupName){
+                    res.status(500).send("This group can not be deleted as it has sub group under")
+                }
+            }
+        })
+
+        conGroup.deleteOne({
+            name : groupName
+        }, (err)=>{
+            if(err){
+                res.status(500).send(err);
+            }else{
+                res.status(200).json({message : "succusfully delted this groups"});
+                return;
+            }
+        })
+        
+    } catch (error) {
+        res.status(500).send(error);
+        
+    }
+});
+
+
+// @route GET
+// Description : get all group name
+router.get("/all", jwt.validateLogin, roleMiddleware, (req, res)=>{
+    try {
+        if(!req.permission){// check permisiion
+            res.status(500).send("You do not have permission for this acction");
+            return;
+        }
+        conGroup.find((err,data)=>{
+            if(err){
+                res.status(500).send(err);
+                return;
+            }else{
+                res.status(200).json({
+                    data : data, message : "kindly check the data recived"
+                })
+            }
+        })
+        
+    } catch (error) {
+
+        res.status(500).send(error);
+        
+    }
 });
