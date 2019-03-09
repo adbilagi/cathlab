c2984a8f764a9838cd8bd3f068dc65967eb47f8b
Dr Umesh R Bilagi <pradyumnabilagi12@gmail.com>
2019-03-09 12:39:36 +0530

    validategroup

--- validate/groupValidate.js
+++ validate/groupValidate.js
@@ -3,7 +3,40 @@
  */ 
 const validator = require("validator");
 const roles = require("../roles/roles");
+const conGroup = require("../model/schema").Group;
+const parentGroup =["Captil Account", "Assets", "Liabilities", "Profit and Loss"];
+
 
 module.exports =(curJson)=>{
-    // write for each fileds of groupSchema mongoose
+   for (const key in curJson) {
+        switch (key) {
+            case "name":
+
+                // to write code
+                break;
+            case "underGroup":
+            if(!(parentGroup.indexOf(curJson.underGroup) >=0)){
+                conGroup.findOne({
+                    name : curJson.underGroup
+                 }, (err, data)=>{
+                     if(err){
+                         throw err;
+                     }else{
+                         if(data[0].underGroup !== curJson.underGroup){
+                             throw "invalid Under group"
+                         }
+                     }
+                 })
+
+            }
+  
+
+
+                break;
+            
+        
+            default:
+                break;
+        }
+   }
 }
