565d1f20b55a66eaea3823942d99261355bcc997
Pradyumna <pradyumnabilagi12@gmail.com>
2019-03-17 00:46:50 +0530

    ledger validadation

--- validate/ledgerValidate.js
+++ validate/ledgerValidate.js
@@ -1,11 +1,20 @@
-const Promise = require("promise")
+// const Promise = require("promise")
 const isEmail = require("validator").isEmail
 
 let email = (email)=>{
     return new Promise((resolve, reject)=>{
-        if(isEmail(email)=== false){
-            
+        if(isEmail(email)==false){
+            reject("invalid email");
+        }else{
+            resolve(true)
         }
     })
+
 }
+
+email("umesh@gmailcom").then((data)=>{
+    console.log(data);
+}).catch((er)=>{
+    console.log(er);
+})
 
