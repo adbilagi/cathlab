4f1246b400424b6104854850cb38acf25ab66b36
Pradyumna <pradyumnabilagi12@gmail.com>
2019-03-17 09:23:45 +0530

    opening balance

--- validate/ledgerValidate.js
+++ validate/ledgerValidate.js
@@ -1,5 +1,5 @@
-// const Promise = require("promise")
 const isEmail = require("validator").isEmail
+
 
 let email = (email)=>{
     return new Promise((resolve, reject)=>{
@@ -11,10 +11,37 @@ let email = (email)=>{
     })
 
 }
+let openingBalance = (ob)=>{
+    let rejectString="Amount has be suffixed with cr or dr for example 500cr or 300dr";
+    return new Promise((resolve, reject)=>{
+        if(typeof ob =="number"){
+            resolve(ob);
+        }else if(typeof ob == "string"){
+            ob= ob.toLowerCase();
+        }else{
+            reject(rejectString);
+            return;
+        }
+        // ====================================
 
-email("umesh@gmailcom").then((data)=>{
-    console.log(data);
-}).catch((er)=>{
-    console.log(er);
-})
+        if( isNaN(Number(ob.substr(0, ob.length-2)))){
+            reject(rejectString);
+        }
+        else if(ob.substr(ob.length-2) ==="cr"){
+            
+        resolve(Math.abs(parseFloat(ob)) * -1)
+            
+        }
+        else if(ob.substr(ob.length-2) ==="dr"){
+            resolve(Math.abs(parseFloat(ob)))
+            
+        }
+        else{
+            reject(rejectString);
+        }
+    })
+}
 
+
+
+openingBalance("234.56cr").then(res=>console.log(res)).catch(err=>console.log(err));
